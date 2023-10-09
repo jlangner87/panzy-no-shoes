25e7ee42e6117f253e897b0c425d735c14fabe02
jlangner87 <josh.langner@gmail.com>
2023-10-09 11:39:50 -0500

    add form components

---
+++ client/src/components/forms/contact.jsx
@@ -0,0 +1,25 @@
+function ContactForm () {
+  return (
+    <div className="contact-form">
+      <form action="submit">
+        <label htmlFor="fname"> First Name</label>
+        <input type="text" name="fname" id="fname" value={"Sample State"} placeholder="First Name"/>
+        <label htmlFor="lname"> Last Name</label>
+        <input type="text" name="lname" id="lname" value={"Sample State"} placeholder="Last Name"/>
+        <br/>
+        <label htmlFor="subject">Subject</label>
+        <select name="subject" id="subject">
+          <option value="">Previous Order Questions</option>
+          <option value="">Product Questions</option>
+          <option value="">Something Else</option>
+        </select>
+        <br/>
+        <label htmlFor="message">Message</label>
+        <textarea name="message" id="message" cols="30" rows="10" placeholder="Youe Message Here"></textarea>
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default ContactForm
