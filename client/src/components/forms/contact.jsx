f242e26b446307bf27978787628522bb7a988c37
jlangner87 <josh.langner@gmail.com>
2023-10-09 12:08:51 -0500

    additional styling for photos //use stock images while awaiting client delivery of image assets

--- client/src/components/forms/contact.jsx
+++ client/src/components/forms/contact.jsx
@@ -2,6 +2,7 @@ function ContactForm () {
   return (
     <div className="contact-form">
       <form action="submit">
+        <h2>Send a Message</h2>
         <label htmlFor="fname"> First Name</label>
         <input type="text" name="fname" id="fname" value={"Sample State"} placeholder="First Name"/>
         <label htmlFor="lname"> Last Name</label>
