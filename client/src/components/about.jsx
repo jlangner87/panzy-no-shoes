ff6222c736da39b5b4793762381b8829e0cd89df
jlangner87 <josh.langner@gmail.com>
2024-01-02 15:42:09 -0600

    add pricing graphics to product descriptor element.

--- client/src/components/about.jsx
+++ client/src/components/about.jsx
@@ -10,16 +10,13 @@ function About () {
       <img className="flourish" src={flourish}/>
       <div className="content-block">
         <h2>What do we do?</h2>
-        <p><em>*This is placeholder Text. I am editing this to include what you sent me the other day*</em></p>
-        <br />
-        <p>Thank you for visiting, and welcome to Panzy No Shoes, where luxury meets self-care! Founded by the Angie York, our brand is all about spreading happiness through aromatherapy experiences. With a commitment to promoting self-care, our products are crafted with love and precision, ensuring a blissful escape from the ordinary.</p>
+        <p>Thank you for visiting, and welcome to Panzy No Shoes, where luxury meets self-care! Founded by Angie York, our brand is all about spreading happiness through aromatherapy experiences. With a commitment to promoting self-care, our products are crafted with love and precision, ensuring a blissful escape from the ordinary.</p>
         <br/>
         <p>From our cheerful blends to the whimsically designed packaging, every detail is crafted to add a sprinkle of joy to your daily routine. Embrace the happiness at Panzy No Shoes, where each product is a delightful invitation to create your own oasis of joy and relaxation.</p>
       </div>
       <div className="content-block">
       <img className="sample" src={samplePhoto2} alt="" />
         <h2>Why do we do it?</h2>
-        <p><em>*This is placeholder Text. I am editing this to include what you sent me the other day*</em></p>
         <p>At Panzy No Shoes, we believe in the transformative power of relaxation as the cornerstone of a healthy and joyous life. Our mission is to enhance well-being by providing premium aromatherapy products that inspire moments of tranquility and promote a balanced lifestyle.</p>
         <br/>
         <p>We are dedicated to crafting blends that not only captivate the senses but also serve as therapeutic tools, fostering a serene space where individuals can unwind, recharge, and find solace.</p>
--- client/src/components/gallery.jsx
+++ client/src/components/gallery.jsx
@@ -1,6 +1,16 @@
 import InstaFeeds from "./instaFeed"
 import flourish from "../assets/flourish.png"
 import downArrow from "../assets/downIcon.png"
+import bbomb from "./forms/formAssets/bbomb.png"
+import sscrub from "./forms/formAssets/sscrub.png"
+import sguide from "./forms/formAssets/sguide.png"
+import lotion from "./forms/formAssets/lotion.png"
+import priceLotion from "../assets/pricing-lotion.png"
+import priceSugar from "../assets/pricing-sugar.png"
+import priceBomb from "../assets/pricing-bomb.png"
+import priceShareBox from "../assets/pricing-sharebox.png"
+import priceSampleBox from "../assets/pricing-samplebox.png"
+
 function Gallery () {
   return (
     <div className="section" id="products">
@@ -12,19 +22,40 @@ function Gallery () {
         <br/>
         <p>Crafted with care, our products feature a harmonious blend of nourishing sweet almond oil, signature essential oil blends, real cane sugar, and vibrant colors.</p>
         <br/>
+        <img src={bbomb} alt="" className="title-img"/>
+        <h2>Bath Bombs</h2>
+        <br />
         <p>Immerse yourself in the soothing embrace of our <h3>Bath Bombs</h3> as they effervesce and release captivating fragrances, turning your bath into a retreat to the spa.</p>
         <br/>
+        <img src={priceBomb} alt="" className="price-img"/>
+        <img src={priceShareBox} alt="" className="price-img"/>
+        <img src={priceSampleBox} alt="" className="price-img"/>
+        <br />
+        <img className="flourish2" src={flourish}/>
+        <img src={sscrub} alt="" className="title-img"/>
+        <h2>Sugar Scrubs</h2>
+        <br />
         <p>Our <h3>Sugar Scrubs</h3>, enriched with sweet almond oil, provide gentle exfoliation, leaving your skin irresistibly soft and radiant.</p>
         <br/>
+        <img src={priceSugar} alt="" className="price-img"/>
+        <br />
+        <img className="flourish2" src={flourish}/>
+        <img src={lotion} alt="" className="title-img"/>
+        <br />
+        <h2>Lotions</h2>
+        <br />
         <p>Complement your routine with our small batch <h3>Lotions</h3>, carefully curated for its velvety texture and luxurious feel, leaving your skin deeply moisturized. </p>
         <br/>
         <p>Elevate your self-care routine with the artistry of our meticulously crafted bath essentials, each one a testament to quality and the joy of indulgence.</p>
-        <br/>
         <p>Treat yourself or a loved one to a sensory escape that revitalizes the body and uplifts the spirit.</p>
         <br />
-        <p><em>I will add the pricing information you sent me here, unless you would like me to add it somewhere else instead</em></p>
+        <img src={priceLotion} alt="" className="price-img"/>
+        <br />
+        <img className="flourish2" src={flourish}/>
+        <br />
       </div>
       <div className="content-block">
+      <img src={sguide} alt="" className="title-img"/>
         <h2>Soul Guides</h2>
         <p>Embark on a transformative journey of self-discovery with our exquisite <h3>Soul Guide</h3> a bespoke, made-to-order book that unveils the mysteries of your existence through the lens of Numerology, Astrology, and Human Design.</p>
         <br/>
@@ -33,6 +64,7 @@ function Gallery () {
         <p>This intricately crafted guide serves as a treasure trove of wisdom, offering a deep reading into the core of your being and tracing the path your soul has walked from the very beginning. With meticulous attention to detail, our Soul Guide is not just a book; it's a sacred journey, a mirror reflecting the essence of who you are.</p>
         <br/>
         <p>Elevate your self-awareness, embrace your authenticity, and navigate life with a newfound clarity. Order your Soul Guide today and illuminate the extraordinary story that is uniquely, beautifully, and authentically yours.</p>
+        <img className="flourish2" src={flourish}/>
       </div>
       <div className="content-block">
         <div className='instagram-display'>
